0bb8618dcfeb9df87a9652b6ce2f4001a0b8da22
authenticBloodshed <robinvos2002@gmail.com>
2024-04-17 17:18:35 +0200

    Added bar and target now bounces

--- Introduction To Godot/Target.gd
+++ Introduction To Godot/Target.gd
@@ -1,6 +1,6 @@
 extends Sprite2D
 
-var random = Vector2(0, randf_range(-10, 10))
+var random = Vector2(0, randf_range(-15, 15))
 var motion = Vector2(0, -1)
 
 # Called when the node enters the scene tree for the first time.
@@ -12,3 +12,6 @@ func _ready():
 func _process(delta):
 	# Applies motion
 	position += random * delta
+	
+	if position.y > 583 or position.y < 65:
+		random *= -1
