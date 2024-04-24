e25646add7d78633ffc823abdaab0ebe1a8cddca
authenticBloodshed <robinvos2002@gmail.com>
2024-04-24 19:10:10 +0200

    trying to disable node (failing badly)

--- Introduction To Godot/WinTimer.gd
+++ Introduction To Godot/WinTimer.gd
@@ -1,19 +1,37 @@
 extends Timer
 class_name WinTimer
 
+var bar_path = "Minigame/MinigameScene/Bar"  # Path to the bar node
+
 func _ready():
 	start()
 	paused = true
+	
+	if not is_inside_tree():
+		return
+
+	# Attempt to get the node
+	var bar = get_node_or_null(bar_path)
+	
+	if bar == null:
+		print("No bar found")
+		return
+
+	disable_node(bar)
 
 func startTimer():
 	paused = false
-	
+
 func pauseTimer():
 	paused = true
-	
+
 func _process(delta):
 	$Label.text = str(round(time_left))
 
-
 func _on_timeout():
-	print("Win")
+	pass
+
+func disable_node(bar):
+	bar.input_disabled = true
+	bar.visible = false
+
