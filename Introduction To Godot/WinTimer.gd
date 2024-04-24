d73b791bb1618da3765025a7c802446588714b18
authenticBloodshed <robinvos2002@gmail.com>
2024-04-24 22:33:24 +0200

    Nodes get disabled when goal is reached

--- Introduction To Godot/WinTimer.gd
+++ Introduction To Godot/WinTimer.gd
@@ -1,6 +1,10 @@
 extends Timer
 class_name WinTimer
 
+@export var player:CharacterBody2D
+@export var bar:Sprite2D
+@export var target:Sprite2D
+@export var timer:Label
 
 func _ready():
 	start()
@@ -18,4 +22,11 @@ func _process(delta):
 
 func _on_timeout():
 	print("Win")
+	player.process_mode = Node.PROCESS_MODE_DISABLED
+	player.visible = false
+	bar.process_mode = Node.PROCESS_MODE_DISABLED
+	bar.visible = false
+	target.process_mode = Node.PROCESS_MODE_DISABLED
+	target.visible = false
+	timer.process_mode = Node.PROCESS_MODE_DISABLED
 
