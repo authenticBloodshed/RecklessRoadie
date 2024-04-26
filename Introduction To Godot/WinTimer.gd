b7ba58827b4eda9276f043990e17466e2327fe90
authenticBloodshed <robinvos2002@gmail.com>
2024-04-26 22:41:07 +0200

    added last bar, added winscreen

--- Introduction To Godot/WinTimer.gd
+++ Introduction To Godot/WinTimer.gd
@@ -1,6 +1,7 @@
 extends Timer
 class_name WinTimer
 
+var barcount = 1
 @export var player:CharacterBody2D
 @export var bar:Sprite2D
 @export var target:Sprite2D
@@ -50,4 +51,9 @@ func _on_timeout():
 	timer2.visible = true
 	collision2.process_mode = Node.PROCESS_MODE_INHERIT
 	area2D2.process_mode = Node.PROCESS_MODE_INHERIT
+	barcount = barcount + 1
+	print(barcount)
+	
+	if barcount == 6:
+		get_tree().change_scene_to_file("res://WinScreen.tscn")
 
