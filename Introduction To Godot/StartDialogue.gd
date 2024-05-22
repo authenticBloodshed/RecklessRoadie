2085b7fcdaa1f73168595ba3ba8383b42915865e
authenticBloodshed <robinvos2002@gmail.com>
2024-05-22 23:26:27 +0200

    gameloop now works

--- Introduction To Godot/StartDialogue.gd
+++ Introduction To Godot/StartDialogue.gd
@@ -1,9 +1,14 @@
-extends Node2D
+extends Button
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	pass
+	
+
+func _on_pressed():
 	Dialogic.start("BeforeShowScene")
+	get_tree().change_scene_to_file("res://TitleScreen.tscn")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
